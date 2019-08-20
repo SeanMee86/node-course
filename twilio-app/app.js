9afe3d645c5b8497c31a02e4489a696659109afb
hss-smee <sean@healthcaresuccess.com>
2019-08-20 16:58:49 -0700

    added twilio project

---
+++ twilio-app/app.js
@@ -0,0 +1,16 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+// DANGER! This is insecure. See http://twil.io/secure
+const accountSid = 'ACbd2db1a9f1abc18d7473c9fc3a4e6b5d';
+const authToken = '576dfd9821ab2801e04e034a2823f491';
+const client = require('twilio')(accountSid, authToken);
+
+
+
+client.messages
+    .create({
+        body: 'Hi there!',
+        from: '+15625267865',
+        to: '+17147263823'
+    })
+    .then(message => console.log(message.sid));

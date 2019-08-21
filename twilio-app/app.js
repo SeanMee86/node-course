64fbd12db247a289c281f84883e96fcbb030e80f
hss-smee <sean@healthcaresuccess.com>
2019-08-21 15:07:43 -0700

    securing auth token for twilio

--- twilio-app/app.js
+++ twilio-app/app.js
@@ -2,7 +2,7 @@
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
 const accountSid = 'ACbd2db1a9f1abc18d7473c9fc3a4e6b5d';
-const authToken = '576dfd9821ab2801e04e034a2823f491';
+const authToken = require('./authToken').token;
 const client = require('twilio')(accountSid, authToken);
 
 

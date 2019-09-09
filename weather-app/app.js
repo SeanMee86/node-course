44b0ba7ae6698b71b67c3df8994f6280149a8227
hss-smee <sean@healthcaresuccess.com>
2019-09-09 11:49:42 -0700

    reverting additions to weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -21,6 +21,21 @@ const geoCodeBaseUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
 const location = 'Los Angeles';
 const geoCodeUrl = `${geoCodeBaseUrl}${location}.json?access_token=pk.eyJ1Ijoic2Vhbm1lZTg2IiwiYSI6ImNrMDZ6emFvbDB4YWMzbmsxcG9wYnV5dGQifQ.3apV4HTjZbPK_MvUDEafKA&limit=1`;
 
+// request({
+//   url: 'https://api.darksky.net/forecast/1d0d2f715fdf13c5f2bacd423967bd08/34.0544,-118.2439',
+//   json: true
+// }, (err, res) => {
+//   if(err){
+//     console.log('Could not connect to Weather Service...')
+//   }else if(res.body.error){
+//     console.log(res.body.error)
+//   }else{
+//     const currentWeather = res.body.currently;
+//     console.log(`${res.body.daily.data[0].summary} It is currently ${currentWeather.temperature} degrees farenheit out and there is a
+//       ${currentWeather.precipProbability}% chance of precipitation`);
+//   }
+// });
+
 request({
   url: geoCodeUrl,
   json: true
@@ -36,20 +51,6 @@ request({
     const latLong = res.body.features[0].center;
     const latitude = latLong[1];
     const longitude = latLong[0];
-    const url = `https://api.darksky.net/forecast/1d0d2f715fdf13c5f2bacd423967bd08/${latitude},${longitude}`;
-    request({
-      url: url,
-      json: true
-    }, (err, res) => {
-      if(err){
-        console.log('Could not connect to Weather Service...')
-      }else if(res.body.error){
-        console.log(res.body.error)
-      }else{
-        const currentWeather = res.body.currently;
-        console.log(`${res.body.daily.data[0].summary} It is currently ${currentWeather.temperature} degrees farenheit out and there is a
-      ${currentWeather.precipProbability}% chance of precipitation`);
-      }
-    });
+    console.log(`${latitude}, ${longitude}`);
   }
 });

c3d6b94fcefcf50d1724e8f6e950420630a14165
hss-smee <sean@healthcaresuccess.com>
2019-09-19 14:04:54 -0700

    cleaning code

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,11 +5,15 @@ const location = process.argv
     .slice(2)
     .join(' ');
 
-geoCode(location, (error, data) => {
-    if(error) return console.log('Error: ', error);
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if(location) {
+    geoCode(location, (error, data) => {
         if(error) return console.log('Error: ', error);
-            console.log(`${data.location}`);
-            console.log(`${forecastData}`);
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error) return console.log('Error: ', error);
+                console.log(`${data.location}`);
+                console.log(`${forecastData}`);
+        });
     });
-});
+}else {
+    console.log('Please provide a location');
+}
